92cf99e7cf0ba90b6c6a70219f26f300225cbc3a
ArcticFox85 <60521197+ArcticFox85@users.noreply.github.com>
2020-08-19 18:36:16 +0300

    1:15

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,48 +1,29 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {
+    Link
+} from 'react-router-dom';
 import data from '../data';
 
 
 function HomeScreen(props) {
-    return <ul className = "products" > {
-            data.products.map(product =>
+    return <ul className = "products" > 
+    {data.products.map(product =>
+    <li>
+<div className = "product">
+< Link to = {'/product/' + product._id}>
+<img className = "product-image" src = {product.image} alt = "product" />
+</Link> 
+<div className = "product-name"> 
+<Link to={'/product/' + product._id}>{product.name}</Link>
+</div>
+<div className = "product-brand" >{product.brand} </div> 
+<div className = "product-price" >${product.price}</div>
+<div className="product-rating"></div>
+        </div>
+        </li>)
+        }
+</ul>
 
-                <
-                li >
-                <
-                div className = "product" >
-
-                <
-                img className = "product-image"
-                src = {
-                    product.image
-                }
-                alt = "product" / >
-                <
-                div className = "product-name" > <Link to={'/product/' + product._id}> {
-                    product.name} </Link></div >
-
-                <
-                div className = "product-brand" > {
-                    product.brand
-                } < /div> <
-                div className = "product-price" > $ {
-                    product.price
-                } < /div> <
-                div className = "product-rating" > {
-                    product.rating
-                }
-                Stars({
-                        product.numReviews
-                    }
-                    s) < /div> <
-                /div>
-
-                <
-                /li>
-            )
-        } <
-        /ul>
 }
 
 
